6969d71f935532f538f10253a36f97575fedb24d
Fábio Mata <fabio.mata@tecnico.ulisboa.pt>
2024-04-27 12:25:08 +0100

    feat(ast): adapt `print_node` to receive several expressions and to append a newline
    Co-authored-by: Nuno Gonçalves <nunomrgoncalves@tecnico.ulisboa.pt>

--- ast/print_node.h
+++ ast/print_node.h
@@ -1,7 +1,7 @@
-#ifndef __SIMPLE_AST_PRINT_NODE_H__
-#define __SIMPLE_AST_PRINT_NODE_H__
+#ifndef __TIL_AST_PRINT_NODE_H__
+#define __TIL_AST_PRINT_NODE_H__
 
-#include <cdk/ast/expression_node.h>
+#include <cdk/ast/sequence_node.h>
 
 namespace til {
 
@@ -9,14 +9,15 @@ namespace til {
    * Class for describing print nodes.
    */
   class print_node : public cdk::basic_node {
-    cdk::expression_node *_argument;
+    cdk::sequence_node *_arguments;
+    bool _add_newline;
 
   public:
-    print_node(int lineno, cdk::expression_node *argument) :
-        cdk::basic_node(lineno), _argument(argument) {
-    }
+    print_node(int lineno, cdk::sequence_node *arguments, bool add_newline = false) :
+        cdk::basic_node(lineno), _arguments(arguments), _add_newline(add_newline) {}
 
-    cdk::expression_node *argument() { return _argument; }
+    cdk::sequence_node *arguments() { return _arguments; }
+    bool add_newline() { return _add_newline; }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_print_node(this, level); }
 
