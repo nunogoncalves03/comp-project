ff81accab375abc3ab2b48111f2e9f23b8054a25
Fábio Mata <fabio.mata@tecnico.ulisboa.pt>
2024-04-27 17:53:09 +0100

    feat(ast): adapt read_node to be an expression
    Co-authored-by: Nuno Gonçalves <nunomrgoncalves@tecnico.ulisboa.pt>

--- ast/read_node.h
+++ ast/read_node.h
@@ -1,6 +1,7 @@
 #ifndef __TIL_AST_READ_NODE_H__
 #define __TIL_AST_READ_NODE_H__
 
+#include <cdk/ast/expression_node.h>
 #include <cdk/ast/lvalue_node.h>
 
 namespace til {
@@ -8,12 +9,12 @@ namespace til {
   /**
    * Class for describing read nodes.
    */
-  class read_node : public cdk::basic_node {
+  class read_node : public cdk::expression_node {
     cdk::lvalue_node *_argument;
 
   public:
     read_node(int lineno, cdk::lvalue_node *argument) :
-        cdk::basic_node(lineno), _argument(argument) {
+        cdk::expression_node(lineno), _argument(argument) {
     }
 
     cdk::lvalue_node *argument() { return _argument; }
