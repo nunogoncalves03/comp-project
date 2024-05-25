6a929cd33aae75c547678b3be1aa7f5b669a1e9d
Fábio Mata <fabio.mata@tecnico.ulisboa.pt>
2024-05-25 11:46:19 +0100

    feat: type check read_node
    Co-authored-by: Nuno Gonçalves <nunomrgoncalves@tecnico.ulisboa.pt>

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -357,11 +357,9 @@ void til::type_checker::do_print_node(til::print_node *const node, int lvl) {
 //---------------------------------------------------------------------------
 
 void til::type_checker::do_read_node(til::read_node *const node, int lvl) {
-  // try {
-  //   node->argument()->accept(this, lvl);
-  // } catch (const std::string &id) {
-  //   throw "undeclared variable '" + id + "'";
-  // }
+  ASSERT_UNSPEC; // Don't override infered type by parent
+
+  node->type(cdk::primitive_type::create(0, cdk::TYPE_UNSPEC));
 }
 
 //---------------------------------------------------------------------------
