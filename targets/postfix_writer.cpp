0f01a73ec846410182f7b52da1fe53bf64d95bf3
Fábio Mata <fabio.mata@tecnico.ulisboa.pt>
2024-05-28 11:22:46 +0100

    feat(postfix_writer): implement read_node
    Co-authored-by: Nuno Gonçalves <nunomrgoncalves@tecnico.ulisboa.pt>

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -362,11 +362,17 @@ void til::postfix_writer::do_print_node(til::print_node * const node, int lvl) {
 //---------------------------------------------------------------------------
 
 void til::postfix_writer::do_read_node(til::read_node * const node, int lvl) {
-  // ASSERT_SAFE_EXPRESSIONS;
-  // _pf.CALL("readi");
-  // _pf.LDFVAL32();
-  // node->argument()->accept(this, lvl);
-  // _pf.STINT();
+  ASSERT_SAFE_EXPRESSIONS;
+
+  if (node->is_typed(cdk::TYPE_DOUBLE)) {
+    _external_functions_to_declare.insert("readd");
+    _pf.CALL("readd");
+    _pf.LDFVAL64();
+  } else {
+    _external_functions_to_declare.insert("readi");
+    _pf.CALL("readi");
+    _pf.LDFVAL32();
+  }
 }
 
 //---------------------------------------------------------------------------
